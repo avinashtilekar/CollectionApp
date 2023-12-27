747cdc52bcf12e9b6d7f3c2a9b11a2455e4cdb9e
Avinash <avinash.devapp@gmail.com>
2023-12-27 23:35:56 +0530

    Remove unwanted code

--- app/src/main/java/com/example/ajspire/collection/view_model/ApiCallViewModel.kt
+++ app/src/main/java/com/example/ajspire/collection/view_model/ApiCallViewModel.kt
@@ -4,15 +4,12 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.ajspire.collection.api.ApiRepository
 import com.example.ajspire.collection.api.AppRetrofitBuilder
 import com.example.ajspire.collection.api.helper.NetworkResult
 import com.example.ajspire.collection.api.model.request.LoginRequest
 import com.example.ajspire.collection.api.model.response.LoginResponse
-import com.example.ajspire.collection.extensions.UserPreferencesRepository
 import kotlinx.coroutines.launch
 
 class ApiCallViewModel(application: Application) : AndroidViewModel(application) {
@@ -28,10 +25,4 @@ class ApiCallViewModel(application: Application) : AndroidViewModel(application)
         }
     }
 
-    class MyViewModelFactory(private val mApplication: Application, private val userPreferencesRepository: UserPreferencesRepository) :
-        ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return DataStoreViewModel(mApplication, userPreferencesRepository) as T
-        }
-    }
 }
