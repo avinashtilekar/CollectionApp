32b732a5126f0f003d822c50a671261c18616f8e
Avinash <avinash.devapp@gmail.com>
2023-12-27 06:38:43 +0530

    ammount selection added

--- app/src/main/java/com/example/ajspire/collection/ui/entry/EntryFragment.kt
+++ app/src/main/java/com/example/ajspire/collection/ui/entry/EntryFragment.kt
@@ -1,18 +1,21 @@
 package com.example.ajspire.collection.ui.entry
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.ajspire.collection.MyApplication
+import com.example.ajspire.collection.R
 import com.example.ajspire.collection.databinding.FragmentEntryBinding
 import com.example.ajspire.collection.room.entity.TransactionTable
+import com.example.ajspire.collection.ui.custom.RadioGridGroup
+import com.example.ajspire.collection.utility.AppUtility
 
 class EntryFragment : Fragment() {
 
@@ -45,9 +48,34 @@ class EntryFragment : Fragment() {
     }
 
     private fun updateUi() {
-        binding.btnSubmit.setOnClickListener {
-            entryViewModel.insert(TransactionTable(fee_type = "Sample", amount ="20", mobile_tran_key = "sjdksjdkj" ))
+        binding.apply {
+            rgFeeType.setOnCheckedChangeListener(object : RadioGridGroup.OnCheckedChangeListener {
+                override fun onCheckedChanged(group: RadioGridGroup?, checkedId: Int) {
+                    AppUtility.hideSoftKeyboard(activity as Activity)
+                    when(checkedId){
+                        R.id.rb24 ->
+                            etAmount.setText(R.string.fee_type_24_amt)
+                        R.id.rb24X48 ->
+                            etAmount.setText(R.string.fee_type_24_48_amt)
+                        R.id.rb48X72 ->
+                            etAmount.setText(R.string.fee_type_48_72_amt)
+                        R.id.rb72X100 ->
+                            etAmount.setText(R.string.fee_type_72_100_amt)
+                    }
+                }
+
+            })
+            btnSubmit.setOnClickListener {
+                entryViewModel.insert(TransactionTable(fee_type = "Sample", amount ="20", mobile_tran_key = "sjdksjdkj" ))
+            }
+            btnCancel.setOnClickListener {
+                etMobileNumber.setText("")
+                etUserName.setText("")
+                rb24.isChecked=true
+            }
         }
+
+
     }
 
 
